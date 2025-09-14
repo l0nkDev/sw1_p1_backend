01796bee426887acaa2b0a34bdcbaf797c0c04fc
l0nkDev <43931415+l0nkDev@users.noreply.github.com>
2025-09-14 14:49:00 -0400

    basic whole diagram syncing

---
+++ sw1_p1_backend/asgi.py
@@ -0,0 +1,15 @@
+"""
+ASGI config for sw1_p1_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+from  sw1_p1_backend import routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sw1_p1_backend.settings')
+
+application = routing.application
---
+++ sw1_p1_backend/consumers.py
@@ -0,0 +1,32 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class DiagramConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["session_name"]
+        self.room_group_name = f"diagram_{self.room_name}"
+        
+        await self.channel_layer.group_add(
+            self.room_group_name, self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Called when the WebSocket closes for any reason.
+        pass
+
+    async def receive(self, text_data):
+        # Called with a message received from the WebSocket.
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        print("received data")
+        print(self.room_group_name)
+        await self.channel_layer.group_send(
+            self.room_group_name, {"type": "chat.message", "message": message}
+        )
+        
+    async def chat_message(self, event):
+        update = event["message"]
+        print(f"sent data")
+        # Send message to WebSocket
+        await self.send(update)
