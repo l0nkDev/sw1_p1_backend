1a2c978ddecf405ccbd12bba5731550009fb066e
l0nkDev <43931415+l0nkDev@users.noreply.github.com>
2025-09-30 13:20:51 -0400

    testing little change

--- sw1_p1_backend/settings.py
+++ sw1_p1_backend/settings.py
@@ -46,19 +46,10 @@ ASGI_APPLICATION = 'sw1_p1_backend.asgi.application'
 
 CHANNEL_LAYERS = {
         'default': {
-            # 'BACKEND': 'channels.layers.InMemoryChannelLayer', # For development
+             'BACKEND': 'channels.layers.InMemoryChannelLayer', # For development
             # 'BACKEND': 'channels_redis.core.RedisChannelLayer', # For production with Redis
             # 'CONFIG': {
             #     'hosts': [('127.0.0.1', 6379)]
-            "BACKEND": "channels_postgres.core.PostgresChannelLayer",
-            "CONFIG": {
-                "ENGINE": "django.db.backends.postgresql_psycopg2", # Or your specific PostgreSQL engine
-                "NAME": "sw1p1db",
-                "USER": "postgres",
-                "PASSWORD": "\\^MGbat%=5deeuN",
-                "HOST": "34.55.58.2",
-                "PORT": "5432"
-            }
         }
 }
 
